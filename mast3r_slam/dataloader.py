8bfa64e4be42d0c77a0e7149b894c80897321326
Charlie <hieuristics@netropy.ai>
2025-02-26 16:01:01 +0900

    fixed dataloader for video

--- mast3r_slam/dataloader.py
+++ mast3r_slam/dataloader.py
@@ -223,10 +223,10 @@ class MP4Dataset(MonocularDataset):
         self.stride = config["dataset"]["subsample"]
 
     def __len__(self):
-        return self.total_frames // self.stride
+        return self.total_frames - self.total_frames % self.stride
 
     def read_img(self, idx):
-        img = self.decoder[idx]  # c,h,w
+        img = self.decoder[idx * self.stride]  # c,h,w
         img = img.permute(1, 2, 0)
         img = img.numpy()
         img = img.astype(self.dtype)
